72a243b0bad23291ec84bf6e21714fb2e2188521
ficlel <victorleal190@gmail.com>
2020-02-20 22:07:44 +0100

    cors

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 const express = require('express')();
 const app = express;
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "script-src 'self' https://apis.google.com");
-    return next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 
